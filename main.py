0378442788629af79b4a4d7da8e09f8e6e739f02
elnukakujo <jagernoe1s3@hotmail.com>
2024-08-04 11:30:30 -0400

    81% testing accuracy for multi class

--- main.py
+++ main.py
@@ -31,17 +31,18 @@ def multi_class(path=False,print_image=False, print_metric=False):
     
     model=multi_classification()
     if not path:
-        parameters, costs, train_accs, test_accs = model.training(x_train,y_train, x_test, y_test, steps=10000, learning_rate=0.008)
+        parameters, costs, train_accs, test_accs = model.training(x_train,y_train, x_test, y_test, steps=6000, learning_rate=0.009)
     else:
         parameters=model.load_weights(path)
-
     print(f"The test accuracy is {np.round(model.compute_accuracy(x_test, y_test)*100)}%")
     
     if print_image:
         index = 0
         display_image(x_train[index], y_train[index])
+    try:
+        if print_metric:
+            display_metrics(costs, train_accs, test_accs)
+    except UnboundLocalError or NameError:
+        print("Costs, train accuracies and/or test accuracies not defined")
         
-    if print_metric:
-        display_metrics(costs, train_accs, test_accs)
-        
-multi_class(print_metric=True)
+multi_class(print_metric=True)#path="step_1_train_acc_0.040933333333333335_cost_2.3025943838385157",
